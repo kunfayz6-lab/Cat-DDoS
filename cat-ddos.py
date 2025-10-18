b498cbee4f91f1876cae681a017c6b5729fa7e40
kunfayz6-lab <kunfayz6@gmail.com>
2025-10-18 08:46:45 +0700

    Update cat-ddos.py

--- cat-ddos.py
+++ cat-ddos.py
@@ -44,7 +44,7 @@ def log_attack_status(message, level='info', print_to_terminal=True):
 def display_header():
     header_lines = [
 f"{Fore.GREEN}╔══════════════════════════════════════════════════════╗",
-f"{Fore.GREEN}║{Fore.CYAN}                *,
+f"{Fore.GREEN}║{Fore.CYAN}                ",
 f"{Fore.GREEN}║{Fore.CYAN}               ,-----,        .---------------     ",
 f"{Fore.GREEN}║{Fore.CYAN}              /       \  __  /    ----------     ",
 f"{Fore.GREEN}║{Fore.CYAN}             / /       \(  )/    --------     ",
